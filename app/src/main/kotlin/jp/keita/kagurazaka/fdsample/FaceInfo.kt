87b77023be3e14efbb216cfbae57705a95a54eca
Keita Kagurazaka <kei.simple@gmail.com>
2016-02-17 03:12:17 +0900

    Improve FaceInfo

--- app/src/main/kotlin/jp/keita/kagurazaka/fdsample/FaceInfo.kt
+++ app/src/main/kotlin/jp/keita/kagurazaka/fdsample/FaceInfo.kt
@@ -4,13 +4,19 @@ import android.graphics.PointF
 import com.google.android.gms.vision.face.Face
 
 data class FaceInfo(
+        var id: Int,
         var leftTop: PointF,
-        var rightBottom: PointF
+        var rightBottom: PointF,
+        var eulerY: Float,
+        var eulerZ: Float
 ) {
     companion object {
         fun create(face: Face): FaceInfo = FaceInfo(
+                face.id,
                 PointF(face.position.x, face.position.y),
-                PointF(face.position.x + face.width, face.position.y + face.height)
+                PointF(face.position.x + face.width, face.position.y + face.height),
+                face.eulerY,
+                face.eulerZ
         )
     }
 }
